47d8734896bb56e41927cbd2e247cc7ca08463e5
Wojciech Pszonak <wojciech.pszonak@gmail.com>
2023-12-29 12:55:36 +0100

    Added 2023_19_2

--- CSharp/2023/Day19/App.cs
+++ CSharp/2023/Day19/App.cs
@@ -4,10 +4,13 @@ using System.Text.RegularExpressions;
 
 public partial class App : Base
 {
+    private const int MaxValue = 4000;
+
     private readonly Dictionary<string, IEnumerable<Rule>> workflows = [];
     private readonly List<Part> parts = [];
     private readonly List<Part> accepted = [];
     private readonly List<Part> rejected = [];
+    private readonly Dictionary<string, IEnumerable<Setup>> cache = [];
 
     public override string[] Parse(string[] input)
     {
@@ -28,8 +31,8 @@ public partial class App : Base
                 var rules = match.Groups[2].Value.Split(',')
                     .Select(x => x.Split(':'))
                     .Select(x => x.Length > 1
-                        ? new Rule(ParseCondition(x[0]), x[1])
-                        : new Rule(part => true, x[0]))
+                        ? new Rule(ParseCondition(x[0]), x[0], x[1])
+                        : new Rule(part => true, null, x[0]))
                     .ToList();
 
                 workflows.Add(workflowName, rules);
@@ -65,6 +68,78 @@ public partial class App : Base
         return Expression.Lambda<Func<Part, bool>>(operation, parameter).Compile();
     }
 
+    private Setup[] Split(Setup setup, char parameter, char @operator, int value)
+    {
+        var result = new Setup[]
+        {
+            setup.Clone(),
+            setup.Clone(),
+        };
+
+        var ranges = parameter switch
+        {
+            'x' => (result[0].X, result[1].X),
+            'm' => (result[0].M, result[1].M),
+            'a' => (result[0].A, result[1].A),
+            's' => (result[0].S, result[1].S),
+            _ => throw new Exception()
+        };
+
+        switch (@operator)
+        {
+            case '>':
+                ranges.Item1.Min = Math.Max(ranges.Item1.Min, value + 1);
+                ranges.Item2.Max = Math.Min(ranges.Item2.Max, value);
+                break;
+            case '<':
+                ranges.Item1.Max = Math.Min(ranges.Item1.Max, value - 1);
+                ranges.Item2.Min = Math.Max(ranges.Item2.Min, value);
+                break;
+        };
+
+        return result;
+    }
+
+    private IEnumerable<Setup> GetSetups(Setup currentSetup, IEnumerable<Rule> rules)
+    {
+        var result = new List<Setup>();
+
+        foreach (var rule in rules)
+        {
+            var destination = rule.Destination;
+
+            if (rule.Raw is not null)
+            {
+                var parameter = rule.Raw[0];
+                var @operator = rule.Raw[1];
+                var value = int.Parse(rule.Raw[2..]);
+
+                var adjustedSetups = Split(currentSetup, parameter, @operator, value);
+
+                if (destination == "A")
+                {
+                    result.Add(adjustedSetups[0]);
+                }
+                else if (destination != "R")
+                {
+                    result.AddRange(GetSetups(adjustedSetups[0], workflows[destination]));
+                }
+
+                currentSetup = adjustedSetups[1];
+            }
+            else if (destination == "A")
+            {
+                result.Add(currentSetup);
+            }
+            else if (destination != "R")
+            {
+                result.AddRange(GetSetups(currentSetup, workflows[destination]));
+            }
+        }
+
+        return result;
+    }
+
     public override object Part1(string[] input)
     {
         foreach (var part in parts)
@@ -95,13 +170,49 @@ public partial class App : Base
 
     public override object Part2(string[] input)
     {
-        var result = workflows.SelectMany(x => x.Value.Select(y => y.Destination));
-        return null!;
+        var result = GetSetups(new Setup(), workflows["in"]);
+
+        return result
+            .Sum(x => (long)x.X.Length * x.M.Length * x.A.Length * x.S.Length);
     }
 
     private record Part(int X, int M, int A, int S);
 
-    private record Rule(Func<Part, bool> Condition, string Destination);
+    private record Rule(Func<Part, bool> Condition, string? Raw, string Destination);
+
+    private class Setup
+    {
+        public Range X { get; set; } = new();
+        public Range M { get; set; } = new();
+        public Range A { get; set; } = new();
+        public Range S { get; set; } = new();
+
+        public Setup Clone()
+            => new()
+            {
+                X = X.Clone(),
+                M = M.Clone(),
+                A = A.Clone(),
+                S = S.Clone()
+            };
+    }
+
+    private class Range
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+
+        public int Length => Max - Min + 1;
+
+        public Range(int min = 1, int max = MaxValue)
+        {
+            Min = min;
+            Max = max;
+        }
+
+        public Range Clone()
+            => new(Min, Max);
+    }
 
     [GeneratedRegex("(\\w+){(.*)}")]
     private static partial Regex WorkflowRegex();
